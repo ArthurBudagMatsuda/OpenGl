ea5e42272e256636d8aae59005e0106d84ad367b
ArthurMatsuda <Arthurmatsudaoficial@gmail.com>
2024-02-28 17:27:29 -0300

    Triangle translate
    trying to understand a little more about VAO and VBOS

--- Origem.cpp
+++ Origem.cpp
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
 #include<GL/glew.h>
 #include<GLFW/glfw3.h>
 
 
-//window dimensions
-const GLint WIDTH = 800, HEIGHT = 600l;
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-GLuint VAO, VBO, shader;
+//window dimensions
+const GLint WIDTH = 1920, HEIGHT = 1080;
+
+
+GLuint VAO[2], VBO[2], shader, uniformMovel;
+bool direction = true;
+float trioOffSet = 0.0f;
+float triMaxOffSet = 0.7f;
+float triIncreament = 0.0005f;
+ /* variable will be a variable which is constant over the whole flow.
+
+We pass in one variable and that variable will be the same on every single instance or every single
+
+time that shader runs for that set of coordinates. */
+ /*//atribite variable  is  is distinct for each point that's passed through.
+
+So each coordinate that gets this shader here will be run for each coordinate that's passed through
+
+each of the points on the triangle.*/
 
 //Vertex Shader
+
 static const char* vShader = "															\n\
 #version 330																			\n\
 layout(location = 0)in vec3 pos;														\n\
+uniform mat4 Model	;																    \n\
+																						\n\
 																						\n\
 void main(){																			\n\
-		gl_Position= vec4(0.2* pos.x,0.2* pos.y,pos.z,1.0);								\n\
+		gl_Position= Model * vec4(0.2* pos.x ,0.2* pos.y,pos.z,1.0);						\n\
 }																						\n\
 ";
 //fragment shader
@@ -24,16 +47,22 @@ static const char* fShader =  "															\n\
 out  vec4 color;																		\n\
 																						\n\
 void main(){																		    \n\
-		color= vec4(1.0,0.0,0.0,1.0);													\n\
+		color= vec4(1.0,1.0,1.0,1.0);													\n\
 }																						 \n\
 ";
-void CreateTriangle() 
+void CreateTriangle()
 {
 	GLfloat vertices[] = {
 		-1.0f,-1.0f,0.0f, //vertice 1 / x -1.0f , y -1.0f , 0.0f
 		1.0f,-1.0f,0.0f,//vertice 2 
-		0.0f,1.0f,0.0f//vertice 3
+		0.0f,1.0f,0.0f,//vertice 3
 	};
+	GLfloat secondTriangle[] = {
+	-1.0f,-1.0f,0.0f, //vertice 1 / x -1.0f , y -1.0f , 0.0f
+	1.0f,-1.0f,0.0f,//vertice 2 
+	0.0f,1.0f,0.0f,//vertice 3
+	};
+
 
 	glGenVertexArrays(1, &VAO);// o graphics card ta criando um vertex array e rervando o espaco
 	glBindVertexArray(VAO);
@@ -41,12 +70,16 @@ void CreateTriangle()
 		glGenBuffers(1, &VBO);
 		glBindBuffer(GL_ARRAY_BUFFER,VBO);
 
-			glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 9, vertices, GL_STATIC_DRAW);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) * 9, vertices, GL_STATIC_DRAW);
+			
+
 			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 			glEnableVertexAttribArray(0);  
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+
 }
+
 void AddShader(GLuint TheProgram,const char*shaderCode,GLenum shaderType) {
 	GLuint theShader = glCreateShader(shaderType);
 	const GLchar* theCode[1];
@@ -90,16 +123,25 @@ void CompileShader() {
 
 	if (!result) {
 		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
-		printf("Error linking program: ' %s' ",eLog);
-			return;
+		printf("Error linking program: ' %s' ", eLog);
+		return;
 	}
+
 	glValidateProgram(shader);
 	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+
 	if (!result) {
 		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
 		printf("Error Validating program: ' %s' ", eLog);
 		return;
 	}
+
+	uniformMovel = glGetUniformLocation(shader, "Model");
+}
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
 int main() {
 
@@ -157,21 +199,39 @@ int main() {
 
 
 	while (!glfwWindowShouldClose(mainWindow)) {
-		//get+ handle user input events
-		glfwPollEvents();
 
-		//clear window
+		if (direction) {
+			trioOffSet += triIncreament;
+		}
+		else {
+			trioOffSet -= triIncreament;
+		}
+
+		if (trioOffSet >= triMaxOffSet || trioOffSet <= -triMaxOffSet) {
+			direction = !direction;
+		}
+
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shader);
+			
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, glm::vec3(trioOffSet, 0.0f, 0.0f));
 
-			glBindVertexArray(VAO);
-				glDrawArrays(GL_TRIANGLES, 0, 3);
-			glBindVertexArray(0);
+		glUniformMatrix4fv(uniformMovel, 1, GL_FALSE, glm::value_ptr(model));
+			
+		glBindVertexArray(VAO);
+		glBindVertexArray(VAO2);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glBindVertexArray(0);
 
 		glUseProgram(0);
 		glfwSwapBuffers(mainWindow);
+		glfwPollEvents();
+
 	}
+
 	return 0;
 }
 
